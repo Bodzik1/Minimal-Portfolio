a5c07909c5d08b53a6514c3ff571fc56fd860cd8
Bogdan <Bogdanmjr@gmail.com>
2024-03-29 17:22:50 -0400

    Picture Issue

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
 	{
 		title: 'Color Gradient Generator',
-		imgUrl: '/assets/ColorGen.png',
+		imgUrl: './assets/ColorGen.png',
 		stack: ['Html, CSS, JavaScript'],
 		link: 'https://bodzik1.github.io/ColorGradientGen/',
 	},
 	{
 		title: 'RoboFriends',
-		imgUrl: '/assets/RoboFriends.png',
+		imgUrl: './assets/RoboFriends.png',
 		stack: ['React'],
 		link: 'https://bodzik1.github.io/RoboFriends/',
 	},
 	{
 		title: 'Landing Page',
-		imgUrl: '/assets/LandingPage.png',
+		imgUrl: './assets/LandingPage.png',
 		stack: ['Html, css'],
 		link: 'https://bodzik1.github.io/Landing-Page/',
 	},
