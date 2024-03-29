f0bcd64dfaecbb68584b36db55e1dab05492cd1d
Bodzik1 <bogdanmjr@gmail.com>
2024-03-29 17:26:55 -0400

    Added Full Name

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ function Intro() {
 	return (
 		<div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
 			<h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
-				Bogdan
+				Bogdan Mieldzicz
 			</h1>
 			<p className="text-base md:text-xl mb-3 font-medium">Game Developer</p>
 			<p className="text-sm max-w-xl mb-6 font-bold">
