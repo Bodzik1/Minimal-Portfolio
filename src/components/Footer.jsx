17433b5345148b6c781431517fa8e702d7847e84
Bogdan <Bogdanmjr@gmail.com>
2024-03-29 02:34:22 -0400

    Changed Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -61,7 +61,7 @@ function Footer() {
 				</div>
 			</div>
 			<p className="text-sm mt-2  opacity-50">
-				&copy; {new Date().getFullYear()} Forrest Knight. All rights reserved.
+				&copy; {new Date().getFullYear()} Bogdan Mieldzicz. All rights reserved.
 			</p>
 		</div>
 	);
